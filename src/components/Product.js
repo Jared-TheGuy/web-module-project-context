5a04bc9096025511a0d1c33d5ce8c093f5e8a8f2
Jared Bates-Baker <51772046+Jared-TheGuy@users.noreply.github.com>
2021-11-04 18:43:24 -0600

    inital setup

--- src/components/Product.js
+++ src/components/Product.js
@@ -4,6 +4,7 @@ const Product = props => {
 	return (
 		<div className="product">
 			<img src={props.product.image} alt={`${props.product.title} book`} />
+			
 
 			<h1 className="title">{props.product.title}</h1>
 
